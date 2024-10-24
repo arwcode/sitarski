c07c4e2728cd7150864d3192c1f00d1176753d4d
fikasp <fikasp@gmail.com>
2024-10-24 21:09:06 +0200

    Style: ArwNav

--- src/components/dialogs/ImageDialog.tsx
+++ src/components/dialogs/ImageDialog.tsx
@@ -75,7 +75,7 @@ export default function ImageDialog({
 					scroll
 					swipe
 					callback={handlePrev}
-					className="absolute top-1/2 -translate-y-1/2 p-1 z-50 left-0 md:left-4 xl:left-1 arw-shadow-white dark:arw-shadow-black"
+					className="absolute top-1/2 -translate-y-1/2 p-1 z-50 left-0 md:left-4 arw-shadow-white dark:arw-shadow-black"
 					size={40}
 				/>
 				<ArwNavNext
@@ -83,7 +83,7 @@ export default function ImageDialog({
 					scroll
 					swipe
 					callback={handleNext}
-					className="absolute top-1/2 -translate-y-1/2 p-1 z-50 right-0 md:right-4 xl:right-1 arw-shadow-white dark:arw-shadow-black"
+					className="absolute top-1/2 -translate-y-1/2 p-1 z-50 right-0 md:right-4 arw-shadow-white dark:arw-shadow-black"
 					size={40}
 				/>
 				<ArwNavClose
